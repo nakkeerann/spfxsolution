cb577b202ce574a7080ec8c4f6e50137f04c826e
Nakkeeran Natarajan <navcse06@gmail.com>
2019-03-18 04:23:52 +0530

    first commit

---
+++ src/webparts/myspfxwp/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMyspfxwpWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MyspfxwpWebPartStrings' {
+  const strings: IMyspfxwpWebPartStrings;
+  export = strings;
+}
